2e8df000d9e19e6d360f9fcd1b7937d49c259f42
Shyam Sundar <Shyam.Muthukumar@albertsons.com>
2022-04-22 09:52:24 +0530

    Added configuration for MongoDB

--- spring/microservices/stocks-management-service/src/main/java/com/stockmarketapp/stocksmanagementservice/StocksManagementServiceApplication.java
+++ spring/microservices/stocks-management-service/src/main/java/com/stockmarketapp/stocksmanagementservice/StocksManagementServiceApplication.java
@@ -1,16 +1,23 @@
 package com.stockmarketapp.stocksmanagementservice;
 
+import com.stockmarketapp.stocksmanagementservice.repository.StockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableSwagger2
 @EnableCaching
+@EnableMongoRepositories
 public class StocksManagementServiceApplication {
+
+	@Autowired
+	StockRepository stockRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(StocksManagementServiceApplication.class, args);
---
+++ spring/microservices/stocks-management-service/src/main/java/com/stockmarketapp/stocksmanagementservice/repository/StockRepository.java
@@ -0,0 +1,7 @@
+package com.stockmarketapp.stocksmanagementservice.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface StockRepository  extends MongoRepository {
+
+}
