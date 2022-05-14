65e2e39cde6151177284c3489a83e92b00f4b0f6
Shyam Sundar <shaamsundar16@gmail.com>
2022-05-14 16:06:27 +0530

    Added Actuator and Sonar configurations

--- spring/microservices/search-stocks-service/src/main/java/com/stockmarketapp/searchstocksservice/SearchStocksServiceApplication.java
+++ spring/microservices/search-stocks-service/src/main/java/com/stockmarketapp/searchstocksservice/SearchStocksServiceApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableSwagger2
 @EnableCaching
 @EnableMongoRepositories
 public class SearchStocksServiceApplication {
